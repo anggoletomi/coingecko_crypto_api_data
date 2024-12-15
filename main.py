c72d3705f7558440635818f68c10555364a2b0dc
anggoletomi <anggoletomi@gmail.com>
2024-12-15 08:59:49 +0700

    add main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from cg_data_a_merge_init import cg_data_a_merge_init
+from cg_data_c_processed import cg_data_c_processed
+
+from bi_function import log_function
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+tasks = [
+    (cg_data_a_merge_init,{'cg_apikey' : os.getenv("COINGECKO_API_KEY"),
+                           'currency' : 'usd',
+                           'decimal_precision' : '6',
+                           'last_x_days' : 30,
+                           'delay_between_request' : 3}),
+    (cg_data_c_processed,{}),
+    
+    ]
+
+log_function(tasks)
